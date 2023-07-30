29e78893a3d699b18263884ebfa1ee1f1d626f84
Jay Saini <jyotsaini@icloud.com>
2023-07-30 04:22:57 -0700

    refactor: :zap: switch methods to macros

--- matrix_diagonalization.c
+++ matrix_diagonalization.c
@@ -18,40 +18,79 @@ void printMatrix( double matrix[N][N] ) {
     }
 }
 
-void initializeMatrix( double m[N][N] ) {
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < N; j++) {
-            m[i][j] = 0;
-        }    
-    }
+#define INITIALIZE_MATRIX(matrix) { \
+    matrix[0][0] = 0; \
+    matrix[0][1] = 0; \
+    matrix[0][2] = 0; \
+    matrix[0][3] = 0; \
+    matrix[1][0] = 0; \
+    matrix[1][1] = 0; \
+    matrix[1][2] = 0; \
+    matrix[1][3] = 0; \
+    matrix[2][0] = 0; \
+    matrix[2][1] = 0; \
+    matrix[2][2] = 0; \
+    matrix[2][3] = 0; \
+    matrix[3][0] = 0; \
+    matrix[3][1] = 0; \
+    matrix[3][2] = 0; \
+    matrix[3][3] = 0; \
 }
 
-void initializeIdentityMatrix( double m[N][N] ) {
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < N; j++) {
-            if(i == j) {
-                m[i][j] = 1;
-            } else {
-                m[i][j] = 0;
-            }
-        }    
-    }
+#define INITIALIZE_IDENTITY_MATRIX(matrix) { \
+    matrix[0][0] = 1; \
+    matrix[0][1] = 0; \
+    matrix[0][2] = 0; \
+    matrix[0][3] = 0; \
+    matrix[1][0] = 0; \
+    matrix[1][1] = 1; \
+    matrix[1][2] = 0; \
+    matrix[1][3] = 0; \
+    matrix[2][0] = 0; \
+    matrix[2][1] = 0; \
+    matrix[2][2] = 1; \
+    matrix[2][3] = 0; \
+    matrix[3][0] = 0; \
+    matrix[3][1] = 0; \
+    matrix[3][2] = 0; \
+    matrix[3][3] = 1; \
 }
 
+#define MATRIX_MULTIPLICATION_ELEMENT(row_num, col_num, element) { \
+    result[row_num][col_num] += m1[row_num][element] * m2[element][col_num]; \
+}
+
+#define MATRIX_MULTIPLICATION_COLUMN(row_num, col_num) { \
+    MATRIX_MULTIPLICATION_ELEMENT(row_num, col_num, 0) \
+    MATRIX_MULTIPLICATION_ELEMENT(row_num, col_num, 1) \
+    MATRIX_MULTIPLICATION_ELEMENT(row_num, col_num, 2) \
+    MATRIX_MULTIPLICATION_ELEMENT(row_num, col_num, 3) \
+}
+
+#define MATRIX_MULTIPLICATION_ROW(row_num) { \
+    MATRIX_MULTIPLICATION_COLUMN(row_num, 0) \
+    MATRIX_MULTIPLICATION_COLUMN(row_num, 1) \
+    MATRIX_MULTIPLICATION_COLUMN(row_num, 2) \
+    MATRIX_MULTIPLICATION_COLUMN(row_num, 3) \
+}
+
+/**
+ *    Multiply an NxN matrix with another NxN matrix, store the output in result[N][N]
+ */
+#define MATRIX_MULTIPLICATION() { \
+    INITIALIZE_MATRIX(result); \
+    \
+    MATRIX_MULTIPLICATION_ROW(0) \
+    MATRIX_MULTIPLICATION_ROW(1) \
+    MATRIX_MULTIPLICATION_ROW(2) \
+    MATRIX_MULTIPLICATION_ROW(3) \
+}
 
 /**
  *    Multiply an NxN matrix with another NxN matrix, store the output in result[N][N]
  */
 void matrixMultiplication( double m1[N][N], double m2[N][N], double result[N][N] ) {
-    initializeMatrix(result);
-
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < N; j++) {
-            for(int k = 0; k < N; k++) {
-                result[i][j] += m1[i][k] * m2[k][j];
-            }
-        }
-    }
+    MATRIX_MULTIPLICATION();
 }
 
 
@@ -80,8 +119,8 @@ void diagonalize( double matrix[N][N] ) {
                 double rightRotation[N][N];
                 double leftRotation[N][N];
 
-                initializeIdentityMatrix(rightRotation);
-                initializeIdentityMatrix(leftRotation);
+                INITIALIZE_IDENTITY_MATRIX(rightRotation);
+                INITIALIZE_IDENTITY_MATRIX(leftRotation);
 
                 leftRotation[iter_i][iter_i] = cos(thetaLeft);
                 leftRotation[iter_i][iter_j] = -sin(thetaLeft);
